ed25620036a1f371f52a1d1731c1abe6ad129882
Zaid Khan <zaidbinasiif468.email@example.com>
2025-09-27 15:07:45 +0500

    Enhance styling and animations for scroll and practical elements; add new utility classes and keyframes for improved UI interactions. And hero section is working perfectly

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,7 +6,27 @@ module.exports = {
     "./src/**/*.{js,ts,jsx,tsx}"
   ],
   theme: {
-    extend: {},
+    extend: {
+      animation: {
+        // 'leftAnRightAnime': 'left-Right 2s 1 cubic-bezier(0.4, 0, 0.2, 1) forwards',
+        'mouseScrollBar': 'mouseScrollBar 3s infinite'
+      },
+      keyframes: {
+        // 'leftAnRightAnime ': {
+        //   '0%': { top: '1400px', left: '1300px' },
+        //   '20%': { top: '1400px', left: '1300px' },
+        //   '40%': { top: '1400px', left: '1300px' },
+        //   '60%': { top: '1400px', left: '1300px' },
+        // },
+        'mouseScrollBar': {
+          '0%': { transform: 'translateY(0px)' },
+          '20%': { transform: 'translateY(-10px)' },
+          '40%': { transform: 'translateY(0px)' },
+          '60%': { transform: 'translateY(-10px)' },
+          '100%': { transform: 'translateY(0px)' },
+        },
+      },
+    },
   },
   plugins: [],
 }
